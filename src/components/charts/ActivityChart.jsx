011f80e2e6f109631900373987254d627fd33cd8
Maxime Falcetta <maxime.falcetta@stagiairesmns.fr>
2025-10-21 11:07:25 +0200

    modified:   src/components/charts/ActivityChart.jsx

--- src/components/charts/ActivityChart.jsx
+++ src/components/charts/ActivityChart.jsx
@@ -15,7 +15,7 @@ export default function ActivityChart({ data }) {
     <ResponsiveContainer width="100%" height={300}>
       <BarChart
         data={data}
-        margin={{ top: 32, right: 40, left: 20, bottom: 24 }} // marge interne du chart
+        margin={{ top: 32, right: 40, left: 20, bottom: 24 }}
       >
         <CartesianGrid strokeDasharray="3 3" vertical={false} />
         <Legend
